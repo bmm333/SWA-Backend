c7c6f21e2e3d90adc0bec24c864ecac96b0f8111
bmm333 <81256935+bmm333@users.noreply.github.com>
2025-09-09 00:23:52 +0000

    a

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ async function bootstrap() {
   }));
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  const port = process.env.PORT;
+  const port = process.env.PORT || 8080;
   const host = '0.0.0.0';
   await app.listen(port, host);
   console.log(`Server running on http://${host}:${port}`);
